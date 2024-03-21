f268137f6dfde92afc9df574198fe82e448f35b0
Yan Wang <39293311+icandle@users.noreply.github.com>
2024-03-21 21:29:17 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,15 @@
 # Evaluation on LSDIR_DIV2K_valid datasets:
 CUDA_VISIBLE_DEVICES=0 python test_demo.py \
-    --data_dir [path to your data dir] \
-    --save_dir [path to your save dir] \
+    --data_dir ../ \
+    --save_dir ../results \
     --model_id 0
+
+# Evaluate PFDNLite on LSDIR_DIV2K_valid datasets:
+CUDA_VISIBLE_DEVICES=0 python test_demo.py \
+    --data_dir ../ \
+    --save_dir ../results \
+    --model_id 1
+
 
 # When Test datasets are included 
 # CUDA_VISIBLE_DEVICES=0 python test_demo.py \
